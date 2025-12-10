60f503e9e0614398c05c48fc22d2b8d52ed271d1
adebankesobanke <adebankesobanke9@gmail.com>
2025-12-10 19:58:42 +0100

    Add updated content for stg_patients.sql

--- Desktop/Projects/HospitalDB_ETL/phase1/sql/staging_tables/stg_patients.sql
+++ Desktop/Projects/HospitalDB_ETL/phase1/sql/staging_tables/stg_patients.sql
@@ -1,12 +1,10 @@
-CREATE TABLE IF NOT EXISTS stg_patients AS
-SELECT
+CREATE TABLE IF NOT EXISTS staging.patients AS
+SELECT DISTINCT
     patient_id,
     INITCAP(first_name) AS first_name,
     INITCAP(last_name) AS last_name,
-    UPPER(gender) AS gender,
-    date_of_birth,
-    REGEXP_REPLACE(phone, '[^0-9]', '') AS phone,
-    LOWER(email) AS email,
-    address,
-    created_at
-FROM raw_patients;
+    gender,
+    dob,
+    branch_id
+FROM raw.patients_raw;
+
